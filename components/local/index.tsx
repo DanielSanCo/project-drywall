d7aa446fdf07885e1b6cf3870ec42f745b591084
DanielSanCo <daniel.santos3210@outlook.com>
2023-06-01 14:02:01 -0300

    com next codes 6

--- components/local/index.tsx
+++ components/local/index.tsx
@@ -30,9 +30,7 @@ const Local = () => {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.8680342639377!2d-43.3776708!3d-22.955086700000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bd97282f3ac31%3A0x49482fa391831b65!2sEstr.%20dos%20Bandeirantes%2C%203736%20-%20Curicica%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2022775-114!5e0!3m2!1spt-BR!2sbr!4v1683309006957!5m2!1spt-BR!2sbr" 
                     width="300" 
                     height="300" 
-                    allowFullScreen="" 
                     loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade"
                 ></iframe>
                 <div className={styles.contato}>
                     <h1>Entrar em Contato</h1>

3028fff6f5087b415f6dcc9159894e8cdb422484
DanielSanCo <daniel.santos3210@outlook.com>
2023-06-01 17:01:24 -0300

    next 1.5

--- components/produtos/index.tsx
+++ components/produtos/index.tsx
@@ -72,7 +72,7 @@ const Produtos = () => {
                                     <div style={{color: 'red', margin: '5px'}}>{item.dispo}</div>
                                 }
                                 <div className={styles.contatoArea}>
-                                        <Link target="_blank" href={`https://web.whatsapp.com/send?phone=552135562929&text=doutordrywall.com/produtos/item/${index}`}>
+                                        <Link target="_blank" href={`https://api.whatsapp.com/send?phone=552135562929&text=Olá gostaria de saber o preço do produto: doutordrywall.com/produtos/item/${index}`}>
                                             <div className={styles.zap}>
                                                 <div>Entrar em Contato</div>
                                                 <Image src="/images/social/whatsapp.png" alt="" />
